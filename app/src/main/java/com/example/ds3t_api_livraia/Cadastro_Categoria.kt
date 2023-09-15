2cf2b3ff7895cc32d9d57c15576e4db9447e93ae
LUIZ GUSTAVO DA SILVA <22282203@dsn127m1246732.cfp127aluno.local>
2023-09-15 13:19:26 -0300

    Iniciando

---
+++ app/src/main/java/com/example/ds3t_api_livraia/Cadastro_Categoria.kt
@@ -0,0 +1,11 @@
+package com.example.ds3t_api_livraia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro_Categoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_categoria)
+    }
+}
